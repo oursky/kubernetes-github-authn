9e9c7f165064a39e1f8ae41b9df9e3b8adbd0b77
AlexB138 <AlexRBarnes@gmail.com>
2017-12-14 16:08:19 -0500

    Teams logic is in place

--- main.go
+++ main.go
@@ -30,12 +30,13 @@ func main() {
 		}
 
 		// Check User
+		ctx := context.Background()
 		ts := oauth2.StaticTokenSource(
 			&oauth2.Token{AccessToken: tr.Spec.Token},
 		)
 		tc := oauth2.NewClient(oauth2.NoContext, ts)
 		client := github.NewClient(tc)
-		user, _, err := client.Users.Get(context.Background(), "")
+		user, _, err := client.Users.Get(ctx, "")
 		if err != nil {
 			log.Println("[Error]", err.Error())
 			w.WriteHeader(http.StatusUnauthorized)
@@ -49,6 +50,26 @@ func main() {
 			return
 		}
 
+		// Gather teams
+		opt := &github.ListOptions{PerPage: 1000}
+		teams_results, _, err := client.Organizations.ListUserTeams(ctx, opt)
+		if err != nil {
+			log.Println("[Error]", err.Error())
+			w.WriteHeader(http.StatusUnauthorized)
+			json.NewEncoder(w).Encode(map[string]interface{}{
+				"apiVersion": "authentication.k8s.io/v1beta1",
+				"kind":       "TokenReview",
+				"status": authentication.TokenReviewStatus{
+					Authenticated: false,
+				},
+			})
+			return
+		}
+		var groups []string
+		for _, team := range teams_results {
+			groups = append(teams, *team.Name)
+		}
+
 		log.Printf("[Success] login as %s", *user.Login)
 		w.WriteHeader(http.StatusOK)
 		trs := authentication.TokenReviewStatus{
@@ -56,6 +77,7 @@ func main() {
 			User: authentication.UserInfo{
 				Username: *user.Login,
 				UID:      *user.Login,
+				Groups:   groups,
 			},
 		}
 		json.NewEncoder(w).Encode(map[string]interface{}{
